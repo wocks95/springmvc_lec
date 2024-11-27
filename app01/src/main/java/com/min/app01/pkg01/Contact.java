13b369632bc43e1ffb2546a1c74c1563f3334e66
wocks95 <rla1995cks@naver.com>
2024-11-27 18:05:05 +0900

    20241127

---
+++ app01/src/main/java/com/min/app01/pkg01/Contact.java
@@ -0,0 +1,14 @@
+package com.min.app01.pkg01;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Contact {
+  private String mobile;
+  private String email;
+  
+  
+  
+}

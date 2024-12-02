6a5cfff20e9a283a560aad82b6aca3125000b6ac
wocks95 <rla1995cks@naver.com>
2024-12-02 18:01:12 +0900

    20241202

--- pkg03_pr/src/main/java/com/min/pkg03/pr/HomeController.java
+++ pkg03_pr/src/main/java/com/min/pkg03/pr/HomeController.java
@@ -1,13 +1,11 @@
-package com.min.pkg03_pr;
+package com.min.pkg03.pr;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
   
